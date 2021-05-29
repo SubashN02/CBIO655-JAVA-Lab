b383211a8338569895482ce4a0c4182a2db4d20f
SUBASH N <84781041+SubashN02@users.noreply.github.com>
2021-05-29 19:05:28 +0530

    Add files via upload
    These java programs are done on 28-05-21 as Exercise 1

---
+++ Subject_percent.java
@@ -0,0 +1,23 @@
+
+public class Subject_percent {
+
+	public static void main(String[] args) {
+		int english,tamil,maths,science,social,total;
+		float perc;
+		english=78;
+		tamil=89;
+		maths=67;
+		science=67;
+		social=88;
+		total=(english+tamil+maths+science+social);
+		perc=(total/5);
+		if(perc>50)
+		{
+			System.out.println("Pass");
+		}	
+		else
+		{
+			System.out.println("Fail");
+		}
+	}
+}
---
+++ dnastore.java
@@ -0,0 +1,10 @@
+
+public class dnastore {
+
+	public static void main(String[] args) {
+		String dna;
+		dna="ATACGATACAA";
+		System.out.println("DNA Sequence: "+dna);
+	}
+
+}
