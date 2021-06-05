74f1d3e27aa460233a6ab03587a474f600536557
SUBASH N <84781041+SubashN02@users.noreply.github.com>
2021-06-05 15:15:29 +0530

    Delete Aminoacid.java

--- Aminoacid.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-public class Aminoacid {
-	public static void main(String[] args) {
-		String amino[]= {"G","A","S","T","C","V","L","I","M","P","F","Y","W","D","E","N","Q","H","K","R"};
-		Arrays.sort(amino);
-		System.out.println("Amino acids in sorted order: "+Arrays.toString(amino));
-	}
-}
